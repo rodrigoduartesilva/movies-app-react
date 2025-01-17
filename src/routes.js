9513450c6e77ed52ef87ed18f0eb3f56f5a74ab6
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2025-01-17 12:00:55 -0300

    feat: card e favoritos implementado

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const { default: Inicio } = require("./pages/Inicio/index.js");
 import Cabecalho from "components/Cabecalho/index.js";
 import Container from "components/Container/index.js";
 import Rodape from "components/Rodape/index.js";
+import FavoritosProvider from "contexts/FavoritosContext.js";
 import Favoritos from "pages/Favoritos/index.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -10,10 +11,12 @@ function AppRoutes() {
         <BrowserRouter>
             <Cabecalho />
             <Container>
-                <Routes>
-                    <Route path="/" element={<Inicio />}></Route>
-                    <Route path="/favoritos" element={<Favoritos />}></Route>
-                </Routes>
+                <FavoritosProvider>
+                    <Routes>
+                        <Route path="/" element={<Inicio />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />}></Route>
+                    </Routes>
+                </FavoritosProvider>
             </Container>
             <Rodape />
         </BrowserRouter>
